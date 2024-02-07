a537f6d15ab38a6daa63c97f55bfd558761fe561
Ergys Hila <ergysmania@gmail.com>
2024-02-07 09:33:34 +0200

    Add devDependencies for DaisyUI and TailwindCSS

---
+++ src/layout/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div class="navbar bg-base-100">
+  <div class="navbar-start">
+
+    <a class="btn btn-ghost text-xl">Spring Boot CRUD</a>
+  </div>
+
+  <div class="navbar-end">
+    <a class="btn">Add User</a>
+    <a class="btn">Edit User</a>
+    <a class="btn">Delete User</a>
+
+  </div>
+</div>
+    )
+}
